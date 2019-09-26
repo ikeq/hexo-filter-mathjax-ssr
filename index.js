55401e4ced842a042e48e57827b9359830a894cb
ikeq <9206414+ikeq@users.noreply.github.com>
2019-09-26 11:48:54 +0800

    fix: run render filter before hexo excerpt filter

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const mathjax = require('./lib/mathjax');
 mathjax.config(hexo.config.mathjax);
 
 hexo.extend.filter.register('before_post_render', mathjax.beforeRender, 9);
-hexo.extend.filter.register('after_post_render', mathjax.render);
+hexo.extend.filter.register('after_post_render', mathjax.render, 9);
